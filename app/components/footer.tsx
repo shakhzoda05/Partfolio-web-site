d1888e11db997c3c5db0069e01145447185e0fc4
shakhzoda05 <shakhzoda05@gmail.com>
2024-11-20 03:12:43 +0500

    changee

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <h2 className="pt-[240px] text-[20px] font-bold pr-12 text-center">Footer</h2>
+    <h2 className="pt-[240px] text-black text-[20px] font-bold pr-12 text-center">Footer</h2>
     </>
   )
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <h2 className="text-[20px] text-white  bg-[#ffffff46] p-6 font-bold pt-10 pr-12 text-center">Header</h2>
+    <h2 className="text-[20px] text-black  bg-black/15 p-6 font-bold pt-10 pr-12 text-center">Header</h2>
     </>
   )
 }
