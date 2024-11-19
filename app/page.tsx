e57d1e8676c3a6f50a755d831030f472d3679d9d
shakhzoda05 <shakhzoda05@gmail.com>
2024-11-20 03:04:20 +0500

    change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <h1 className="text-[40px] font-bold pt-[230px] pr-12 text-center">
-        Hozircha ma'lumotlar mavjud emas
+        Hozircha malumotlar mavjud emas
       </h1>
       <br />
       <h2 className="text-[20px] font-normal pt-5 pr-12 text-center">
